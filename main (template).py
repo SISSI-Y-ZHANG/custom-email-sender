90a27a6d7d7a782d0d324cb3839ed728ffad4c34
Sissi Zhang <156721167+SISSI-Y-ZHANG@users.noreply.github.com>
2024-05-06 11:50:55 -0500

    Update main (template).py

--- main (template).py
+++ main (template).py
@@ -1,75 +1,75 @@
-# Gmail Email Sender
-# Author: Sissi Zhang
-
 import os
 from email.message import EmailMessage
 import ssl
 import smtplib
 
+################################################# SENDER #################################################
 
-email_sender = 'example_email@gmail.com'
-email_password = 'password you set up'
-# notice: you'll have to turn on 2-step verification of your gmail, and go to
-# https://myaccount.google.com/apppasswords
-# to set up a 16-character password 
+email_sender = "example_email@gmail.com"
+email_password = "the a 16-character password you set up"
 
-# there is also a website showing the steps: 
-# https://support.google.com/mail/answer/185833?hl=en
-
+################################################# CONTENT #################################################
 
 subject = "Example Email Headline"
 body = '''
-<p style="font-size: 16px; font-family: Cambria">
-Dear {name},
-<br><br>
-
-Thank you for reading into my codes! This sample body will serve as a brief introduction 
-of how you should write the body of your customized email!
-<br><br>
-
-Basically, the line-breaks inside python will not be recognized or executed once the email 
-is sent. You will have to use the html command, <br>, to create a new line. Notice that 
-whenever I switch to a new line inside python, I always leave a space at the end of the line 
-so that the last word and the first word won't merge together.
-<br><br>
-
-You can also add a <a href="https://www.w3schools.com/html/html_links.asp">hyperlink</a> using 
-html command. If you want any more customized text, search for the implementation in html!
-<br><br>
-
-Hope you enjoy!
-<br><br>
-
-Sissi Zhang
-<br><br><br>
+<p style="font-size: 16px; font-family: Arial">
+Dear {name}, <br>
+<br>
+{0} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra 
+maecenas accumsan lacus vel facilisis. <br>
+<br>
+{1} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra 
+maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra 
+maecenas accumsan lacus vel facilisis. <br>
+<br>
+Sincerely, <br>
+Your Name <br>
 '''
 
-recipients_file_path = r'C:\Users\...\recipients.txt'
+################################################# ATTACHMENT #################################################
+
+attachment_path = None
+# the name for the attachment here should match the actual file
+#attachment_path = r'C:\Users\...\picture template.png'
+attachment_type = "image"
+attachment_subtype = "png"
+# the name for the attachment here is the one that will appear on the recipient's email
+attachment_name = "Cat Image"
+
+################################################# RECEIVERS #################################################
+
+recipients_path = r'C:\Users\...\recipients.txt'
 
 recipients = []
 
-with open(recipients_file_path, 'r') as recipients_file:
-    recipient_data = recipients_file.readline()
-    while recipient_data:
-        if recipient_data.startswith('#'):
-            recipient_data = recipients_file.readline()
+with open(recipients_path, 'r') as recipients_file:
+    line = recipients_file.readline()
+    while line:
+        if line.startswith('#') or line == "\n":
+            line = recipients_file.readline()
             continue
         else:
-            name, email = recipient_data.split(';')
-            recipients.append({'name':name.strip(), 'email':email.strip()})
-            recipient_data = recipients_file.readline()
+            temp_storage = line.split(';')
+            name, email = temp_storage[0], temp_storage[1]
+            messages = []
+            if len(temp_storage > 2):
+                for message in temp_storage[2:]:
+                    messages.append(message)
+            recipients.append({"name":name.strip(), "email":email.strip(), "message":messages})
+            line = recipients_file.readline()
 
 # this is how this list will look like:
 '''
 recipients = [
-    {'name': 'Recipient 1', 'email': 'example1@email.address'},
-    {'name': 'Recipient 2', 'email': 'example2@email.address'},
+    {"name": 'Name 1', "email": 'name1@gmail.com', "message": [...]},
+    {"name": 'Name 2', "email": 'name2@gmail.com', "message": [...]},
     # Add more recipients as needed
 ]'''
 
-# the name for the attachment here should match the actual file
-attachment_file_path = r'C:\Users\...\picture template.png'
-
+################################################# MAIN #################################################
 
 def main():
 
@@ -89,7 +89,7 @@ def main():
         with open(attachment_file_path, 'rb') as attachment_file:
             attachment_data = attachment_file.read()
             em.add_attachment(attachment_data, maintype='image', subtype='png', filename='Cat Image')
-            # the name for the attachment here is the one that will appear on the recipient's email
+            
 
         context = ssl.create_default_context()
 
@@ -102,4 +102,39 @@ def main():
 
 
 if __name__ == '__main__':
+    main()
+def main():
+    for person in recipients:
+        # setting up the email
+        body_formatted = body.format(name = person["name"]) 
+        email_receiver = person["email"]
+
+        if messages != []:
+            for i in len(messages):
+                body_formatted = body_formatted.format(i = messages[i])
+                # "{0} {1}".format(10, 20) 
+
+        em = EmailMessage()
+        em["From"] = email_sender
+        em["To"] = email_receiver
+        em["Subject"] = subject
+        em.set_content(body_formatted, "html")
+
+        #sending the email
+        if attachment_path != None: 
+            with open(attachment_path, "rb") as attachment_file:
+                attachment_data = attachment_file.read()
+                em.add_attachment(attachment_data, maintype=attachment_type, \
+                                  subtype=attachment_subtype, filename=attachment_name)
+
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+            smtp.login(email_sender, email_password)
+            smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+        print(person["name"], "done")
+
+
+if __name__ == "__main__":
     main()
